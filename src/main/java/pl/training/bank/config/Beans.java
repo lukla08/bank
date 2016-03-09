00204a077c368a29112c981ac2a65990ad917c12
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-03-09 14:20:28 +0100

    init

--- src/main/java/pl/training/bank/config/Beans.java
+++ src/main/java/pl/training/bank/config/Beans.java
@@ -3,13 +3,16 @@ package pl.training.bank.config;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Scope;
+import pl.training.bank.operation.ConsoleOperationLogger;
 import pl.training.bank.service.AccountNumberGenerator;
 import pl.training.bank.service.AccountsService;
 import pl.training.bank.service.IncrementalAccountNumberGenerator;
 import pl.training.bank.service.repository.AccountsRepository;
 import pl.training.bank.service.repository.HashMapAccountsRepository;
 
+@EnableAspectJAutoProxy
 @Configuration
 public class Beans {
 
@@ -27,6 +30,11 @@ public class Beans {
     @Bean(initMethod = "init", destroyMethod = "destroy")
     public AccountsService accountsService(AccountsRepository accountsRepository, AccountNumberGenerator accountNumberGenerator) {
         return new AccountsService(accountsRepository, accountNumberGenerator);
+    }
+
+    @Bean
+    public ConsoleOperationLogger operationLogger() {
+        return new ConsoleOperationLogger();
     }
 
 }
---
+++ src/main/java/pl/training/bank/operation/ConsoleOperationLogger.java
@@ -0,0 +1,44 @@
+package pl.training.bank.operation;
+
+import org.aspectj.lang.annotation.*;
+import pl.training.bank.BankException;
+
+import java.text.NumberFormat;
+
+@Aspect
+public class ConsoleOperationLogger {
+
+    private static final String SEPARATOR = "####################################################################";
+
+    @Pointcut("execution(void pl.training.bank.service.AccountsService.process(..)) && args(operation)")
+    public void processOperation(Operation operation) {
+    }
+
+    @Before("processOperation(operation)")
+    public void beforeProcessOperation(Operation operation) {
+        System.out.println(SEPARATOR);
+        if (operation instanceof DepositOperation) {
+            System.out.format("%s <== %s\n", operation.getSourceAccountNumber(), formatCurrency(operation.getFunds()));
+        } else if (operation instanceof WithdrawOperation) {
+            System.out.format("%s ==> %s\n", operation.getSourceAccountNumber(), formatCurrency(operation.getFunds()));
+        } else if (operation instanceof TransferOperation) {
+            System.out.format("%s ==> %s ==> %s\n", operation.getSourceAccountNumber(), formatCurrency(operation.getFunds()),
+                    operation.getDestinationAccountNumber());
+        }
+    }
+
+    @AfterReturning("processOperation(operation)")
+    public void onSuccess(Operation operation) {
+        System.out.format("Status: SUCCESS\n%s\n", SEPARATOR);
+    }
+
+    @AfterThrowing(value = "processOperation(operation)", throwing = "ex")
+    public void onException(Operation operation, BankException ex) {
+        System.out.format("Status: EXCEPTION (%s)\n%s\n", ex.getClass().getSimpleName(), SEPARATOR);
+    }
+
+    private String formatCurrency(long value) {
+        return NumberFormat.getCurrencyInstance().format((double) value / 100);
+    }
+
+}
