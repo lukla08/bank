b32e12d1608c1a67bfb751da4d56df12ec480a67
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-03-10 11:27:11 +0100

    init

--- src/main/java/pl/training/bank/config/Persistence.java
+++ src/main/java/pl/training/bank/config/Persistence.java
@@ -1,18 +1,26 @@
 package pl.training.bank.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import pl.training.bank.service.repository.AccountsRepository;
 import pl.training.bank.service.repository.MySQLAccountsRepository;
 
 import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
 
 @PropertySource("classpath:jdbc.properties")
 @EnableTransactionManagement
@@ -23,13 +31,33 @@ public class Persistence {
     private Environment env;
 
     @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUsername(env.getProperty("database.user"));
+    public DataSource dataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setUser(env.getProperty("database.user"));
         dataSource.setPassword(env.getProperty("database.password"));
-        dataSource.setUrl(env.getProperty("database.url"));
-        dataSource.setDriverClassName(env.getProperty("database.driverClass"));
+        dataSource.setJdbcUrl(env.getProperty("database.url"));
+        dataSource.setDriverClass(env.getProperty("database.driverClass"));
+
+        dataSource.setMinPoolSize(10);
+        dataSource.setMaxPoolSize(20);
+        dataSource.setMaxIdleTime(5000);
         return dataSource;
+    }
+
+    @Bean
+    public PropertiesFactoryBean hibernateProperties() {
+        PropertiesFactoryBean factoryBean = new PropertiesFactoryBean();
+        factoryBean.setLocation(new ClassPathResource("hibernate.properties"));
+        return factoryBean;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource, Properties hibernateProperties) {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(hibernateProperties);
+        factoryBean.setPackagesToScan("pl.training.bank.entity");
+        return factoryBean;
     }
 
     @Bean
@@ -38,8 +66,8 @@ public class Persistence {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
+    public PlatformTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
     }
 
 }
--- src/main/resources/insert.sql
+++
@@ -1,8 +0,0 @@
-create database bank;
-use bank;
-create table accounts (
-    id int auto_increment,
-    number varchar(26),
-    balance bigint,
-    primary key(id)
-);
