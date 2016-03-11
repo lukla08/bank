07f3b73f1c6ced16193c4f45d5d60952a61074ca
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-03-11 14:26:09 +0100

    init

--- src/main/java/pl/training/bank/config/Beans.java
+++ src/main/java/pl/training/bank/config/Beans.java
@@ -1,7 +1,10 @@
 package pl.training.bank.config;
 
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import pl.training.bank.operation.*;
 import pl.training.bank.service.AccountNumberGenerator;
 import pl.training.bank.service.AccountsService;
@@ -10,8 +13,7 @@ import pl.training.bank.service.repository.AccountsRepository;
 
 import javax.persistence.EntityManagerFactory;
 
-@Import({Persistence.class, Web.class})
-@EnableAspectJAutoProxy
+@EnableJpaRepositories("pl.training.bank.service.repository")
 @Configuration
 public class Beans {
 
--- src/main/java/pl/training/bank/config/Web.java
+++ src/main/java/pl/training/bank/config/Web.java
@@ -4,25 +4,17 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import pl.training.bank.dto.CustomObjectMapper;
 
 import java.util.List;
 
-@ComponentScan("pl.training.bank.controller")
-@EnableWebMvc
 @Configuration
 public class Web extends WebMvcConfigurerAdapter {
 
@@ -42,28 +34,6 @@ public class Web extends WebMvcConfigurerAdapter {
                 });
     }
 
-    @Bean
-    public ServletContextTemplateResolver templateResolver() {
-        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML5");
-        return templateResolver;
-    }
-
-    @Bean
-    public SpringTemplateEngine templateEngine(ServletContextTemplateResolver templateResolver) {
-        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
-        return templateEngine;
-    }
-
-    @Bean
-    public ThymeleafViewResolver viewResolver(SpringTemplateEngine templateEngine) {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine);
-        return viewResolver;
-    }
 
     @Bean
     public MessageSource messageSource() {
